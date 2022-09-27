e36a693ae57925b4f5b19d0f60ed04e5e4ef0a93
Durgesh Yadav <durgeshyadav3107@gmail.com>
2022-09-27 20:24:20 +0530

    12 : Organise and Combine suites_____Organize JUnits into Suites

---
+++ JunitCourse/src/test/java/com/dgsh/helper/AllTests.java
@@ -0,0 +1,11 @@
+package com.dgsh.helper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ QuckBeforeAfterTest.class, StringHelperTest.class })
+public class AllTests {
+
+}
